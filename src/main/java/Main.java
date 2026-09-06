e1bf94a0d64bf7ba45fb07c248d01ff22f0bf832
Aryan Pachandi <pachandiaryan@gmail.com>
2026-09-06 17:54:26 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
 
 public class Main {
@@ -28,15 +31,20 @@ public class Main {
       }
       case "cat-file" -> {
         final String hash = args[1];
-        final File objectFile = new File(".git/objects", hash);
-    
-        if (!objectFile.exists()) {
+        // final File objectFile = new File(".git/objects", hash);
+        String dirhash = hash.substring(0, 2);
+        String filehash = hash.substring(2);
+        File blobFile = new File(".git/objects/" + dirhash + "/" + filehash);
+
+        if (!blobFile.exists()) {
           System.out.println("Object not found: " + hash);
           return;
         }
     
         try {
-          final byte[] content = Files.readAllBytes(objectFile.toPath());
+          String blob = new BufferedReader(new InputStreamReader(new FileInputStream(blobFile))).readLine();
+          String content = blob.substring(blob.indexOf("\0") + 1);
+          // final byte[] content = Files.readAllBytes(blobFile.toPath());
           System.out.write(content);
         } catch (IOException e) {
           throw new RuntimeException(e);
